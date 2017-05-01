76d75eba3f89ecd3e7f3f07c6e528136cc3fd69a
takanashi66 <inoue@codecodeweb.com>
2017-05-01 20:39:56 +0900

    修正

--- common/js/es/app.js
+++ common/js/es/app.js
@@ -1,15 +1,14 @@
 var app = new Vue({
-  
+
   el: '#app',
-  
+
   data: {
     url: 'http://rest.dev/',
-    wp: [],
     posts: [],
     media: [],
     errors: []
   },
-  
+
   methods: {
     media: function(id){
       axios.get(this.url+'/wp-json/wp/v2/media/'+id)
@@ -19,21 +18,13 @@ var app = new Vue({
       .catch(e => {
         this.errors.push(e)
       });
-      return 'http://codecodeweb.com/wp-content/themes/newCodeCode/common/img/taku.jpg';
+      return '';
     }
   },
-  
+
   //インスタンスが生成されたときに呼び出される
   created(){
-    //wp-json取得
-    axios.get(this.url+'wp-json/')
-    .then(response => {
-      this.wp = response.data
-    })
-    .catch(e => {
-      this.errors.push(e)
-    });
-    
+
     //postを取得
     axios.get(this.url+'wp-json/wp/v2/posts/')
     .then(response => {
@@ -42,6 +33,7 @@ var app = new Vue({
     .catch(e => {
       this.errors.push(e)
     });
+
   },
-  
+
 })
