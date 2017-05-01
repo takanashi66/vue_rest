16c1de76c35c60654b0f50bc94ceb97d466ef460
takanashi66 <inoue@codecodeweb.com>
2017-05-01 17:43:33 +0900

    記事一覧

---
+++ common/js/es/app.js
@@ -0,0 +1,46 @@
+var app = new Vue({
+  
+  el: '#app',
+  
+  data: {
+    url: 'http://rest.dev/',
+    wp: [],
+    posts: [],
+    media: [],
+    errors: []
+  },
+  
+  methods: {
+    media: function(id){
+      axios.get(this.url+'/wp-json/wp/v2/media/'+id)
+      .then(response => {
+        this.media = response.data
+      })
+      .catch(e => {
+        this.errors.push(e)
+      });
+    }
+  },
+  
+  //インスタンスが生成されたときに呼び出される
+  created(){
+    //wp-json取得
+    axios.get(this.url+'wp-json/')
+    .then(response => {
+      this.wp = response.data
+    })
+    .catch(e => {
+      this.errors.push(e)
+    });
+    
+    //postを取得
+    axios.get(this.url+'wp-json/wp/v2/posts/')
+    .then(response => {
+      this.posts = response.data
+    })
+    .catch(e => {
+      this.errors.push(e)
+    });
+  },
+  
+})
---
+++ index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="keywords" content="">
+  <meta name="description" content="">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width">
+  <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/common/css/style.css">
+  <title>Document</title>
+  <?php wp_head(); ?>
+</head>
+<body>
+  
+  <div id="app">
+    <h1>記事一覧</h1>
+    
+    <!-- jsonを整形して出力 -->
+    <p v-if="posts && posts.length">
+      <pre v-for="post of posts"><code>{{post}}</code></pre>
+    </p>
+   
+    
+    <!-- 投稿を出力 -->
+    <div class="post_list" v-if="posts && posts.length">
+      <!-- ここからループ -->
+      <div class="item" v-for="post of posts">
+        <h2><a v-bind:href="post.link">{{post.title.rendered}}</a></h2>
+          {{post.excerpt.rendered}}
+      </div>
+      <!-- ここまでループ -->
+    </div>
+    
+    <!-- エラーを出力 -->
+    <ul v-if="errors && errors.length">
+      <li v-for="error of errors">{{error.message}}</li>
+    </ul>
+    
+    
+  </div>
+  <!-- /#app -->
+  
+  <script src="https://unpkg.com/vue"></script>
+  <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
+  <script src="<?php echo get_template_directory_uri(); ?>/common/js/script.js"></script>
+  <?php wp_footer(); ?>
+</body>
+</html>
